bedb2f3e2c4bd45d5decfb8bd50f1dfa7741450f
Jody8888 <3540115546@qq.com>
2026-03-02 19:20:13 +0800

    Initial Commit

---
+++ Widgetly/WidgetlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetlyApp.swift
+//  Widgetly
+//
+//  Created by Jody on 2026/3/2.
+//
+
+import SwiftUI
+
+@main
+struct WidgetlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
